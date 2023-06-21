992b4fef63fd76be05c18e4b2e111d5669d7420f
MatthewShelby <matthewshelb@gmail.com>
2023-06-21 09:28:14 +0330

    firstOkTest

--- app.js
+++ app.js
@@ -1,123 +1,46 @@
 const express = require("express");
 const app = express();
-//const fetch = require("node-fetch-native"); // Import the fetch library for making HTTP requests
-
+const cors = require('cors');
+const https = require('https');
+var f = require('./funcs')
+var r = require('./reqs')
+//app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const port = process.env.PORT || 3001;
 
 
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
-app.get('/c', async (req, res) => {
-  try {
-    const apiResponse = await fetch('https://api.1inch.io/v5.0/56/approve/allowance?tokenAddress=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&walletAddress=0x11677b07C9AcA203A9131571a164C3F0d3f31908')
-    //const apiResponseJson = await apiResponse.json()
-
-    console.log(apiResponse);
-
-    return res.status(200).json({
-      status: "success", data: apiResponse
-    });
-
-    // console.log(apiResponseJson)
-    // res.send('Running 🏃')
-  } catch (err) {
-    console.log(err)
-    res.status(500).send('Something went wrong')
-  }
-})
-
-app.get('/cc', async (req, res) => {
-  try {
-    var tokenAddress = req.tokenAddress// "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
-    var walletAddress = req.walletAddress//'0x11677b07C9AcA203A9131571a164C3F0d3f31908'
-    var cid = req.cid
-    const apiResponse = await fetch('https://api.1inch.io/v5.0/' + cid + '/approve/allowance?tokenAddress=' + tokenAddress + '&walletAddress=' + walletAddress)
-    //const apiResponseJson = await apiResponse.json()
-    console.log(apiResponse);
-
-    return res.status(200).json({
-      status: "success", data: apiResponse
-    });
-    // console.log(apiResponseJson)
-    // res.send('Running 🏃')
-  } catch (err) {
-    console.log(err)
-    res.status(500).send('Something went wrong')
-  }
-})
 
 
-
-var fs = require('fs');
-//app.get("/", (req, res) => res.type('html').send(html));
-app.get('/', function (request, response) {
-  //response.sendFile('C:/VSCode Repos/DEX/Moralisish/dexstarter/backapp/backapp/htmls/index.html');
-  response.status(200).json({ status: 'success' })
-});
-
-app.get('/al', async (req, res) => {
-  var xx = await fetch('https://api.1inch.io/v5.0/' + req.query)
-  return res.status(200).json({
-    status: "success", data: xx
-  });
-})
-app.get('/als', async (req, res) => {
-  var xx = await fetch('https://api.1inch.io/v5.0/56/approve/allowance?tokenAddress=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&walletAddress=0x11677b07C9AcA203A9131571a164C3F0d3f31908')
-  return res.status(200).json({
-    status: "success", data: xx
-  });
-})
-
-app.get('/al2', async (req, res) => {
-  try {
-    await fetch('https://api.1inch.io/v5.0/' + req.query).then((resx) => {
-      console.log('als2: ------>')
-      console.log(resx)
+app.get("/priceFor/:chain/:payToken/:receiveToken", async (req, res) => {
+  var SellTokenAddress = f.getTokenAddress(req.params.chain, req.params.payToken)
+  var BuyToeknAddress = f.getTokenAddress(req.params.chain, req.params.receiveToken)
+  var url = 'https://bsc.api.0x.org/swap/v1/quote?buyToken=' + BuyToeknAddress + '&sellToken=' + SellTokenAddress + '&sellAmount=20000000000000000'
+  console.log(url)
+  https.get(url, (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => { data += chunk })
+    resp.on('end', () => {
       return res.status(200).json({
-        status: "success", data: resx
+        status: "success", data: (JSON.parse(data))
       });
-    }).catch((error) => {
-      console.log('als2: error ------>')
-      console.log(error)
-      return res.status(200).json({
-        status: "error", data: error
-      });
-    })
-
-  } catch (error) {
-    console.log('als2: error2 ------>')
-    console.log(error)
-    return res.status(200).json({
-      status: "error2", data: error
     });
-  }
-})
-
-
-app.get('/als2', async (req, res) => {
-  try {
-    await fetch('https://api.1inch.io/v5.0/56/approve/allowance?tokenAddress=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&walletAddress=0x11677b07C9AcA203A9131571a164C3F0d3f31908').then((resx) => {
-      console.log('als2: ------>')
-      console.log(resx)
-      return res.status(200).json({
-        status: "success", data: resx
-      });
-    }).catch((error) => {
-      console.log('als2: error ------>')
-      console.log(error)
-      return res.status(501).json({
-        status: "error", data: error
-      });
-    })
-
-  } catch (error) {
-    console.log('als2: error2 ------>')
-    console.log(error)
+  }).on("error", (err) => {
     return res.status(501).json({
-      status: "error2", data: error
+      status: "error", data: err.message
     });
-  }
+  })
 })
+
+
+
+
+
+
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
